8c8f52d0451fe0614689363d0b8869071006ec5e
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-25 22:18:09 +0700

    Refactoring Your Components memecahkan jadi beberapa file

--- public/components/Greeter.js
+++
@@ -1,6 +0,0 @@
-function greeter(){
-    document.write('<h1>From greeter function (yoi menjadi Satu)!</h1>');
-}
-
-
-module.exports = greeter;
---
+++ public/components/Greeter.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm');
+
+
+var Greeter = React.createClass({
+        getDefaultProps: function(){
+                return{
+                        name: 'React',
+                        message: 'This is default message'
+                };
+        },
+
+        getInitialState:function(){
+                return {
+                        name: this.props.name,
+                        message: this.props.message
+                };
+        },
+                
+        handleNewData: function(updates){
+                this.setState(updates);
+                // this.props.message = 'something new';
+        },
+
+    render: function(){
+            var name = this.state.name;
+            var message = this.state.message;
+
+        return(
+          <div>
+            <GreeterMessage name={name} message= {message}/>
+            <GreeterForm onNewData={this.handleNewData}/>
+          </div>
+           )
+        }
+});
+
+module.exports = Greeter;
